18ff6e6f187d6bb4a87482be16dc3fa18a7d6a41
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2021-06-24 16:18:14 -0500

    Initial config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      colors: {
+        primary: '#3AB8FF',
+        grayprim: '#F5F5F5',
+        graysec: '#D9D9D9',
+        grayterc: '#ECECEC',
+        graycuart: '#FCFCFC',
+      }
+    },
+  },
+  plugins: [],
+ }
