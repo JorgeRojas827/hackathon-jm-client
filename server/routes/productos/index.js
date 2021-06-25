48fe6065e122f5e21910be185dc18df06a85a20e
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2021-06-25 01:45:58 -0500

    Deleting unused server

--- server/routes/productos/index.js
+++
@@ -1,7 +0,0 @@
-const { getProducts }= require('../../controllers/products');
-const express = require('express');
-const app = express();
-
-app.get('/productos/show', getProducts);
-
-module.exports = app;
